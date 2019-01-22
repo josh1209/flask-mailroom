ff71e45e28eb9059c9b79f8b27ee4aa481ceb8c9
Josh Hoff <jhoff1985@hotmail.com>
2019-01-22 15:17:48 -0800

    added a form for creating new donations for existing donors, and navigation elements

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -11,13 +11,26 @@ app = Flask(__name__)
 def home():
     return redirect(url_for('all'))
 
-@app.route('/donations/')
+@app.route('/donations/', methods=['GET', 'POST'])
 def all():
-    donations = Donation.select()
-    return render_template('donations.jinja2', donations=donations)
+#    donations = Donation.select()
+    if request.method == 'POST':
+        return redirect(url_for('create'))
+    return render_template('donations.jinja2', donations=Donation.select())
+    
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        if not request.form['name']:
+            return redirect(url_for('all'))
+        donor_name = Donor.select().where(Donor.name == request.form['name']).get()
+        task2 = Donation(value=request.form['donation'], donor=donor_name)
+        task2.save()
+        return redirect(url_for('all'))
+    else:
+        return render_template('create.jinja2')
     
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
